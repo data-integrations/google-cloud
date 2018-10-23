14cecf7a43112cf4888a19495c07b9e01dde662d
albertshau <ashau@google.com>
2018-10-23 13:30:50 -0700

    CDAP-14542 add a property for the dataset project
    Added an optional property for the dataset project that can be
    used if it does not reside in the project in which the job will
    be run.
    
    Also fixing warning messages

--- src/main/java/co/cask/gcp/bigquery/BigQuerySourceConfig.java
+++ src/main/java/co/cask/gcp/bigquery/BigQuerySourceConfig.java
@@ -20,7 +20,9 @@ import co.cask.cdap.api.annotation.Description;
 import co.cask.cdap.api.annotation.Macro;
 import co.cask.cdap.api.annotation.Name;
 import co.cask.cdap.api.data.schema.Schema;
+import co.cask.gcp.common.GCPConfig;
 import co.cask.gcp.common.GCPReferenceSourceConfig;
+import com.google.cloud.ServiceOptions;
 
 import java.io.IOException;
 import javax.annotation.Nullable;
@@ -32,25 +34,53 @@ public final class BigQuerySourceConfig extends GCPReferenceSourceConfig {
   @Macro
   @Description("The dataset the table belongs to. A dataset is contained within a specific project. "
     + "Datasets are top-level containers that are used to organize and control access to tables and views.")
-  public String dataset;
+  private String dataset;
 
   @Macro
   @Description("The table to read from. A table contains individual records organized in rows. "
     + "Each record is composed of columns (also called fields). "
     + "Every table is defined by a schema that describes the column names, data types, and other information.")
-  public String table;
+  private String table;
 
   @Macro
   @Nullable
   @Description("The Google Cloud Storage bucket to store temporary data in. "
     + "It will be automatically created if it does not exist, but will not be automatically deleted. "
-    + "Temporary data will be deleted after it has been read. " +
-    "If it is not provided, a unique bucket will be created and then deleted after the run finishes.")
-  public String bucket;
+    + "Temporary data will be deleted after it has been read. "
+    + "If it is not provided, a unique bucket will be created and then deleted after the run finishes. "
+    + "The service account must have permission to create buckets in the configured project.")
+  private String bucket;
 
   @Macro
   @Description("The schema of the table to read.")
-  public String schema;
+  private String schema;
+
+  @Macro
+  @Nullable
+  @Description("The project the dataset belongs to. This is only required if the dataset is not "
+    + "in the same project that the BigQuery job will run in. If no value is given, it will default to the configured "
+    + "project ID.")
+  private String datasetProject;
+
+  public String getDataset() {
+    return dataset;
+  }
+
+  public String getTable() {
+    return table;
+  }
+
+  @Nullable
+  public String getBucket() {
+    return bucket;
+  }
+
+  public String getDatasetProject() {
+    if (GCPConfig.AUTO_DETECT.equalsIgnoreCase(datasetProject)) {
+      return ServiceOptions.getDefaultProjectId();
+    }
+    return datasetProject == null ? getProject() : datasetProject;
+  }
 
   /**
    * @return the schema of the dataset
