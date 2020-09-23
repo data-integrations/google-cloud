f114da8424ecbea84fc6d3fe206dc12067f7d1a3
Supriya Patil <supriyapatil@google.com>
2020-09-23 12:12:32 -0700

    (PLUGIN-395) - Avoid underlying BigQuery schema changes when allowSchemaRelaxation is disabled in BigQuery sink plugins.

--- src/main/java/io/cdap/plugin/gcp/bigquery/sink/AbstractBigQuerySink.java
+++ src/main/java/io/cdap/plugin/gcp/bigquery/sink/AbstractBigQuerySink.java
@@ -264,6 +264,64 @@ public abstract class AbstractBigQuerySink extends BatchSink<StructuredRecord, A
   }
 
   /**
+   * Adjusts output format schema depending on allowSchemaRelaxation setting to avoid unexpected
+   * schema changes to the underlying bigQuery table.
+   * @param configuredSchema Schema configured for output format.
+   * @param tableName BigQuery table name for writing record.
+   * @param collector Failure collector to report failures to the client.
+   * @return
+   */
+  protected final Schema overrideOutputSchemaWithTableSchemaIfNeeded(
+      Schema configuredSchema,
+      String tableName,
+      FailureCollector collector) {
+    AbstractBigQuerySinkConfig config = getConfig();
+
+    if (!config.isAllowSchemaRelaxation()) {
+      Schema tableSchema = getTableSchema(config.getTable(), collector);
+      // We use GCS buckets to write AVRO files and import them in BigQuery.
+      // Avro is a self describing format and BigQuery overwrites table schema with AVRO record
+      // schema.
+      // If table schema relaxation is not allowed then AVRO records will be normalized and
+      // written to GCS using table schema to avoid any table schema changes.
+      if (tableSchema != null) {
+        LOG.info("Output schema updated to use table schema");
+        return tableSchema;
+      }
+    }
+    return configuredSchema;
+  }
+
+  /**
+   * Returns Bigtable schema in a CDAP schema format.
+   * @param tableName BigQuery table name for writing record.
+   * @param collector Failure collector to report failures to the client.
+   * @return
+   */
+  @Nullable
+  private Schema getTableSchema(String tableName, FailureCollector collector) {
+    AbstractBigQuerySinkConfig config = getConfig();
+    Table table = BigQueryUtil.getBigQueryTable(
+        config.getProject(),
+        config.getDataset(), tableName,
+        config.getServiceAccountFilePath(),
+        collector);
+
+    if (table == null) {
+      LOG.info("Table [%s] doesn't exist yet. Using input schema for writing records.", config.getTable());
+      return null;
+    }
+
+    com.google.cloud.bigquery.Schema bqSchema = table.getDefinition().getSchema();
+    if (bqSchema == null || bqSchema.getFields().isEmpty()) {
+      // Table is created without schema, so no further validation is required.
+      LOG.info("Table [%s] doesn't have a valid schema. Using input schema for writing records.", config.getTable());
+      return null;
+    }
+    return BigQueryUtil.getTableSchema(bqSchema, collector);
+  }
+
+  /**
    * Validates output schema against Big Query table schema. It throws {@link IllegalArgumentException}
    * if the output schema has more fields than Big Query table or output schema field types does not match
    * Big Query column types unless schema relaxation policy is allowed.
@@ -282,32 +340,28 @@ public abstract class AbstractBigQuerySink extends BatchSink<StructuredRecord, A
       return;
     }
 
-    if (getConfig().isTruncateTableSet()) {
-      //no validation required for schema if truncate table is set.
-      // BQ will overwrite the schema for normal tables when write disposition is WRITE_TRUNCATE
-      //note - If write to single partition is supported in future, schema validation will be necessary
-      return;
-    }
-
     FieldList bqFields = bqSchema.getFields();
     List<Schema.Field> outputSchemaFields = Objects.requireNonNull(tableSchema.getFields());
 
     List<String> missingBQFields = BigQueryUtil.getSchemaMinusBqFields(outputSchemaFields, bqFields);
 
     if (allowSchemaRelaxation) {
-      List<String> nonNullableFields = missingBQFields.stream()
-        .map(tableSchema::getField)
-        .filter(Objects::nonNull)
-        .filter(field -> !field.getSchema().isNullable())
-        .map(Schema.Field::getName)
-        .collect(Collectors.toList());
+      // Required fields can be added only if truncate table option is set.
+      if (!getConfig().isTruncateTableSet()) {
+        List<String> nonNullableFields = missingBQFields.stream()
+            .map(tableSchema::getField)
+            .filter(Objects::nonNull)
+            .filter(field -> !field.getSchema().isNullable())
+            .map(Schema.Field::getName)
+            .collect(Collectors.toList());
 
-      for (String nonNullableField : nonNullableFields) {
-        collector.addFailure(
-          String.format("Required field '%s' does not exist in BigQuery table '%s.%s'.",
-                        nonNullableField, getConfig().getDataset(), tableName),
-          "Change the field to be nullable.")
-          .withInputSchemaField(nonNullableField).withOutputSchemaField(nonNullableField);
+        for (String nonNullableField : nonNullableFields) {
+          collector.addFailure(
+              String.format("Required field '%s' does not exist in BigQuery table '%s.%s'.",
+                  nonNullableField, getConfig().getDataset(), tableName),
+              "Change the field to be nullable.")
+              .withInputSchemaField(nonNullableField).withOutputSchemaField(nonNullableField);
+        }
       }
     } else {
       // schema should not have fields that are not present in BigQuery table,
@@ -322,7 +376,9 @@ public abstract class AbstractBigQuerySink extends BatchSink<StructuredRecord, A
       // validate the missing columns in output schema are nullable fields in BigQuery
       List<String> remainingBQFields = BigQueryUtil.getBqFieldsMinusSchema(bqFields, outputSchemaFields);
       for (String field : remainingBQFields) {
-        if (bqFields.get(field).getMode() != Field.Mode.NULLABLE) {
+        Field.Mode mode = bqFields.get(field).getMode();
+        // Mode is optional. If the mode is unspecified, the column defaults to NULLABLE.
+        if (mode != null && mode != Field.Mode.NULLABLE) {
           collector.addFailure(String.format("Required Column '%s' is not present in the schema.", field),
                                String.format("Add '%s' to the schema.", field));
         }
