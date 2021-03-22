4852836c769ff7e7185acfcd933c4afb520f49ab
yaojie <yaojie@google.com>
2021-03-22 15:30:43 -0700

    a

--- src/main/java/io/cdap/plugin/gcp/bigquery/source/BigQueryConnector.java
+++ src/main/java/io/cdap/plugin/gcp/bigquery/source/BigQueryConnector.java
@@ -3,6 +3,9 @@ package io.cdap.plugin.gcp.bigquery.source;
 import io.cdap.cdap.api.annotation.Name;
 import io.cdap.cdap.api.annotation.Plugin;
 
+/**
+ *
+ */
 @Name("BigQuery")
 @Plugin(type = "connector")
 public class BigQueryConnector {
