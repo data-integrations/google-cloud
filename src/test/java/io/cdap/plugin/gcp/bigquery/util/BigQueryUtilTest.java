e76e84b9afc86cd7f711941f908fbe28fc8a964d
Victor Duvert <victor.duvert.ext@veolia.com>
2021-08-11 16:17:31 +0200

    update unit test

--- src/test/java/io/cdap/plugin/gcp/bigquery/util/BigQueryUtilTest.java
+++ src/test/java/io/cdap/plugin/gcp/bigquery/util/BigQueryUtilTest.java
@@ -1,11 +1,19 @@
 package io.cdap.plugin.gcp.bigquery.util;
 
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.LegacySQLTypeName;
+import com.google.cloud.bigquery.StandardSQLTypeName;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.etl.api.FailureCollector;
 import io.cdap.cdap.etl.api.validation.ValidationFailure;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.List;
+import javax.validation.constraints.AssertTrue;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 public class BigQueryUtilTest {
 
@@ -17,11 +25,13 @@ public class BigQueryUtilTest {
                 Schema.Field.of(arrayField, Schema.nullableOf(Schema.arrayOf(Schema.of(Schema.Type.STRING)))));
 
         FailureCollector collector = Mockito.mock(FailureCollector.class);
+        Mockito.when(collector.addFailure(anyString(), anyString())).thenReturn(new ValidationFailure("errorMessage"));
 
-        ValidationFailure failure  = BigQueryUtil.validateArraySchema(
-                schema.getField(arrayField).getSchema(), arrayField, collector
-        );
+        Field bigQueryField = Field.newBuilder(arrayField, StandardSQLTypeName.STRING).setMode(Field.Mode.REPEATED).build();
+        Schema.Field recordField = schema.getField(arrayField);
 
-        Assert.assertNull(failure);
+        BigQueryUtil.validateFieldModeMatches(bigQueryField,recordField, false, collector);
+
+        Mockito.verify(collector,Mockito.times(0)).addFailure(anyString(), anyString());
     }
 }
