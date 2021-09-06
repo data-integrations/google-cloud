f655135bf72bc5c212aad7bb95f5bafd2afdb226
vikasrathee-cs <vikas.rathee@cloudsufi.com>
2021-09-06 18:38:19 +0530

    commit changes for runtime changes of dataplex sink.

---
+++ src/main/java/io/cdap/plugin/gcp/dataplex/sink/DataplexOutputCommitter.java
@@ -0,0 +1,150 @@
+package io.cdap.plugin.gcp.dataplex.sink;
+
+import com.google.cloud.storage.Blob;
+import com.google.common.annotations.VisibleForTesting;
+import io.cdap.plugin.gcp.common.GCPUtils;
+import io.cdap.plugin.gcp.gcs.StorageClient;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.JobStatus;
+import org.apache.hadoop.mapreduce.OutputCommitter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * OutputCommitter for Dataplex
+ */
+public class DataplexOutputCommitter extends OutputCommitter {
+
+  public static final String RECORD_COUNT_FORMAT = "recordcount.%s";
+  private static final Logger LOG = LoggerFactory.getLogger(DataplexOutputFormatProvider.class);
+  private final OutputCommitter delegate;
+
+  public DataplexOutputCommitter(OutputCommitter delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void setupJob(JobContext jobContext) throws IOException {
+    delegate.setupJob(jobContext);
+  }
+
+  @Override
+  public void cleanupJob(JobContext jobContext) throws IOException {
+    delegate.cleanupJob(jobContext);
+  }
+
+  @Override
+  public void commitJob(JobContext jobContext) throws IOException {
+    delegate.commitJob(jobContext);
+  }
+
+  @Override
+  public void abortJob(JobContext jobContext, JobStatus.State state) throws IOException {
+    delegate.abortJob(jobContext, state);
+  }
+
+  @Override
+  public void setupTask(TaskAttemptContext taskAttemptContext) throws IOException {
+    delegate.setupTask(taskAttemptContext);
+  }
+
+  @Override
+  public boolean needsTaskCommit(TaskAttemptContext taskAttemptContext) throws IOException {
+    return delegate.needsTaskCommit(taskAttemptContext);
+  }
+
+  @Override
+  public void commitTask(TaskAttemptContext taskAttemptContext) throws IOException {
+      /*On commit task, there seems to be some inconsistency across different hadoop implementations regarding the path
+       where output file is stored. For some implementations it appears in the path returned by FileOutputCommitter
+       getCommittedTaskPath and for some it does not.Before commit, the files appear to be consistently present in path
+       returned by FileOutputCommitter getTaskAttemptPath. Hence, find the output file from taskAttemptPath and add
+       metadata before commit happens. After commit, file would have been moved out of the taskAttemptPath. */
+    try {
+      updateMetricMetaData(taskAttemptContext);
+    } catch (Exception exception) {
+      LOG.warn("Unable to record metric for task. Metric emitted for the number of affected rows may be incorrect.",
+        exception);
+    }
+
+    delegate.commitTask(taskAttemptContext);
+  }
+
+  private void updateMetricMetaData(TaskAttemptContext taskAttemptContext) throws IOException {
+    if (!(delegate instanceof FileOutputCommitter)) {
+      return;
+    }
+
+    FileOutputCommitter fileOutputCommitter = (FileOutputCommitter) delegate;
+    Configuration configuration = taskAttemptContext.getConfiguration();
+    //Task is not yet committed, so should be available in attempt path
+    Path taskAttemptPath = fileOutputCommitter.getTaskAttemptPath(taskAttemptContext);
+    if (configuration == null || taskAttemptPath == null) {
+      return;
+    }
+
+    //read the count from configuration
+    String keyInConfig = String.format(RECORD_COUNT_FORMAT, taskAttemptContext.getTaskAttemptID());
+    Map<String, String> metaData = new HashMap<>();
+    metaData.put("recordcount", String.valueOf(configuration.getLong(keyInConfig, 0L)));
+    StorageClient storageClient = getStorageClient(configuration);
+    //update metadata on the output file present in the directory for this task
+    Blob blob = storageClient.pickABlob(taskAttemptPath.toString());
+    if (blob == null) {
+      LOG.info("Could not find a file in path {} to apply count metadata.", taskAttemptPath.toString());
+      return;
+    }
+    //to-do
+//    blob.toBuilder().setContentType(configuration.get(DataplexBatchSink.CONTENT_TYPE)).setMetadata(metaData).build()
+//      .update();
+  }
+
+  @VisibleForTesting
+  StorageClient getStorageClient(Configuration configuration) throws IOException {
+    String project = configuration.get(GCPUtils.FS_GS_PROJECT_ID);
+    String serviceAccount = null;
+    boolean isServiceAccountFile = GCPUtils.SERVICE_ACCOUNT_TYPE_FILE_PATH
+      .equals(configuration.get(GCPUtils.SERVICE_ACCOUNT_TYPE));
+    if (isServiceAccountFile) {
+      serviceAccount = configuration.get(GCPUtils.CLOUD_JSON_KEYFILE, null);
+    } else {
+      serviceAccount = configuration.get(String.format("%s.%s", GCPUtils.CLOUD_JSON_KEYFILE_PREFIX,
+        GCPUtils.CLOUD_ACCOUNT_JSON_SUFFIX));
+    }
+    return StorageClient.create(project, serviceAccount, isServiceAccountFile);
+  }
+
+  @Override
+  public void abortTask(TaskAttemptContext taskAttemptContext) throws IOException {
+    delegate.abortTask(taskAttemptContext);
+  }
+
+  @Override
+  public boolean isCommitJobRepeatable(JobContext jobContext) throws IOException {
+    return delegate.isCommitJobRepeatable(jobContext);
+  }
+
+  @Override
+  public boolean isRecoverySupported(JobContext jobContext) throws IOException {
+    return delegate.isRecoverySupported(jobContext);
+  }
+
+  @Override
+  public boolean isRecoverySupported() {
+    return delegate.isRecoverySupported();
+  }
+
+  @Override
+  public void recoverTask(TaskAttemptContext taskContext) throws IOException {
+    delegate.recoverTask(taskContext);
+  }
+}
+
---
+++ src/main/java/io/cdap/plugin/gcp/dataplex/sink/DataplexOutputFormatProvider.java
@@ -0,0 +1,144 @@
+package io.cdap.plugin.gcp.dataplex.sink;
+
+import com.google.common.collect.ImmutableMap;
+import io.cdap.cdap.api.data.format.StructuredRecord;
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.etl.api.validation.FormatContext;
+import io.cdap.cdap.etl.api.validation.ValidatingOutputFormat;
+import io.cdap.plugin.gcp.bigquery.sink.BigQueryOutputFormat;
+import io.cdap.plugin.gcp.bigquery.util.BigQueryConstants;
+import io.cdap.plugin.gcp.bigquery.util.BigQueryUtil;
+import io.cdap.plugin.gcp.gcs.sink.GCSOutputFormatProvider;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.OutputCommitter;
+import org.apache.hadoop.mapreduce.OutputFormat;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * OutputFormatProvider for Dataplex
+ */
+public class DataplexOutputFormatProvider implements ValidatingOutputFormat {
+  public static final String RECORD_COUNT_FORMAT = "recordcount.%s";
+  public static final String DATAPLEX_ASSET_TYPE = "dataplexsink.assettype";
+  private static final String OUTPUT_FOLDER = "dataplexsink.metric.output.folder";
+  private final ValidatingOutputFormat delegate;
+  private final Map<String, String> configMap;
+  private final Configuration configuration;
+  private final Schema tableSchema;
+
+  public DataplexOutputFormatProvider(Configuration configuration, Schema tableSchema,
+                                      ValidatingOutputFormat delegate, Map<String, String> configMap) {
+  //for BQ assets
+    this.configuration = configuration;
+    this.tableSchema = tableSchema;
+  //for GCS assets
+    this.delegate = delegate;
+    this.configMap = configMap == null ? null : ImmutableMap.copyOf(configMap);
+  }
+
+
+  @Override
+  public void validate(FormatContext context) {
+    delegate.validate(context);
+  }
+
+  @Override
+  public String getOutputFormatClassName() {
+    return DataplexOutputFormatProvider.DataplexOutputFormat.class.getName();
+  }
+
+  @Override
+  public Map<String, String> getOutputFormatConfiguration() {
+    if (delegate == null) {
+      Map<String, String> configToMap = BigQueryUtil.configToMap(configuration);
+      if (tableSchema != null) {
+        configToMap
+          .put(BigQueryConstants.CDAP_BQ_SINK_OUTPUT_SCHEMA, tableSchema.toString());
+      }
+      return configToMap;
+    }
+    Map<String, String> outputFormatConfiguration = new HashMap<>(delegate.getOutputFormatConfiguration());
+    return outputFormatConfiguration;
+  }
+
+  /**
+   * OutputFormat for Dataplex Sink
+   */
+  public static class DataplexOutputFormat extends OutputFormat<Object, Object> {
+    private final OutputFormat<NullWritable, StructuredRecord> gcsDelegateFormat =
+      new GCSOutputFormatProvider.GCSOutputFormat();
+    private final OutputFormat<StructuredRecord, NullWritable> bqDelegateFormat = new BigQueryOutputFormat();
+    private OutputFormat delegateFormat;
+
+    private OutputFormat getDelegateFormatInstance(Configuration configuration) throws IOException {
+      if (delegateFormat != null) {
+        return delegateFormat;
+      }
+      String assetType = configuration.get(DATAPLEX_ASSET_TYPE);
+      if (assetType.equalsIgnoreCase(DataplexBatchSink.BIGQUERY_DATASET_ASSET_TYPE)) {
+        delegateFormat = bqDelegateFormat;
+      } else {
+        delegateFormat = gcsDelegateFormat;
+      }
+      return delegateFormat;
+    }
+
+    @Override
+    public RecordWriter<Object, Object> getRecordWriter(TaskAttemptContext taskAttemptContext) throws
+      IOException, InterruptedException {
+      RecordWriter originalWriter = getDelegateFormatInstance(taskAttemptContext.getConfiguration())
+        .getRecordWriter(taskAttemptContext);
+      return new DataplexOutputFormatProvider.DataplexRecordWriter(originalWriter);
+    }
+
+    @Override
+    public void checkOutputSpecs(JobContext jobContext) throws IOException, InterruptedException {
+      getDelegateFormatInstance(jobContext.getConfiguration()).checkOutputSpecs(jobContext);
+    }
+
+    @Override
+    public OutputCommitter getOutputCommitter(TaskAttemptContext taskAttemptContext) throws IOException,
+      InterruptedException {
+      OutputCommitter delegateCommitter = getDelegateFormatInstance(taskAttemptContext.getConfiguration())
+        .getOutputCommitter(taskAttemptContext);
+      return new DataplexOutputCommitter(delegateCommitter);
+    }
+  }
+
+  /**
+   * RecordWriter for DataplexSink
+   */
+  public static class DataplexRecordWriter extends RecordWriter<Object, Object> {
+
+    private final RecordWriter originalWriter;
+    private long recordCount;
+
+    public DataplexRecordWriter(RecordWriter originalWriter) {
+      this.originalWriter = originalWriter;
+    }
+
+    @Override
+    public void write(Object keyOut, Object valOut) throws IOException,
+      InterruptedException {
+      originalWriter.write(keyOut, valOut);
+      recordCount++;
+    }
+
+    @Override
+    public void close(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
+      originalWriter.close(taskAttemptContext);
+      //Since the file details are not available here, pass the value on in configuration
+      taskAttemptContext.getConfiguration()
+        .setLong(String.format(RECORD_COUNT_FORMAT, taskAttemptContext.getTaskAttemptID()), recordCount);
+    }
+  }
+}
+
