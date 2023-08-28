6d56d7cd688d19c5c8788fd18637a4e39cd45c8c
Aryan-Verma <aryanverma@google.com>
2023-08-28 10:08:31 +0530

    fixing unit tests
    fixing error message
    
    fixed DataplexBatchSourceConfigTest
    
    fixed error message

--- src/e2e-test/java/io/cdap/plugin/utils/E2ETestConstants.java
+++ src/e2e-test/java/io/cdap/plugin/utils/E2ETestConstants.java
@@ -7,6 +7,7 @@ public class E2ETestConstants {
   public static final String ERROR_MSG_GCS_INVALID_PATH = "errorMessageGCSInvalidPath";
   public static final String ERROR_MSG_GCS_INVALID_BUCKET_NAME = "errorMessageGCSInvalidBucketName";
   public static final String ERROR_MSG_INCORRECT_TABLE = "errorMessageIncorrectBQTable";
+  public static final String ERROR_MSG_INCORRECT_TABLE_NAME = "errorMessageIncorrectBQTableName";
   public static final String ERROR_MSG_PUBSUB_INVALID_ADVANCED_FIELDS = "errorMessagePubSubInvalidAdvancedField";
   public static final String ERROR_MSG_PUBSUB_MAX_BATCH_COUNT = "errorMessagePubSubMaxBatchCountField";
   public static final String ERROR_MSG_PUBSUB_MAX_BATCH_SIZE = "errorMessagePubSubMaxBatchSizeField";
